ca330cc13ac932564f21d403a92067e40fa5e749
Testin-it <84230658+Testin-it@users.noreply.github.com>
2022-11-18 14:40:58 +0100

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -7,7 +7,7 @@ sys.path.append(str(Path(__file__).parent.parent))
 
 from streamlit_app.page_functions.team_stats import team_statistics
 from streamlit_app.page_functions.player_stats import player_statistics
-from streamlit_app.page_functions.statistics import f_statistics
+from streamlit_app.page_functions.statistics import Dataset_Statistics
 
 path_to_teams = Path(__file__).parent.parent / "data" / "pickles" / "teams.pkl"
 path_to_models = Path(__file__).parent.parent / "data" / "pickles" / "arima"
@@ -43,7 +43,7 @@ page_names_to_funcs = {
     "Homepage": main_page,
     "Player Information": player_statistics,
     "Team Information": team_statistics,
-    "Statistics": f_statistics,
+    "Statistics": Dataset_Statistics,
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
