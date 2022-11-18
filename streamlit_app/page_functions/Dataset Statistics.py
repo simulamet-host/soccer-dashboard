e71c5135c3688ee6e35014826c186e61b74e9cfe
Testin-it <84230658+Testin-it@users.noreply.github.com>
2022-11-18 13:46:33 +0100

    Update and rename statistics.py to Dataset Statistics.py

---
+++ streamlit_app/page_functions/Dataset Statistics.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+import seaborn as sns
+import streamlit as st
+import datetime
+
+st.set_page_config(page_title='Dataset Statistics')
+st.markdown("## Dataset Statistics")
