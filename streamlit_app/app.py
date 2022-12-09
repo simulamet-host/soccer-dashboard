f680d19ffff98a8bc9a793fc8cca624c405b902d
Testin-it <84230658+Testin-it@users.noreply.github.com>
2022-12-09 14:24:11 +0100

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -47,4 +47,4 @@ page_names_to_funcs = {
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page](teams, models)
+page_names_to_funcs[selected_page]
