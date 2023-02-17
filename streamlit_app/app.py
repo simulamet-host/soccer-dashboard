a6ca5f801d4e0e2e317dbc53984acb5dcfede8fa
Testin-it <84230658+Testin-it@users.noreply.github.com>
2023-02-17 13:39:06 +0100

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -5,8 +5,8 @@ import os
 import pickle
 sys.path.append(str(Path(__file__).parent.parent))
 
-from streamlit_app.page_functions.team_stats import team_statistics
-from streamlit_app.page_functions.player_stats import player_statistics
+from streamlit_app.page_functions.team_information import team_statistics
+from streamlit_app.page_functions.player_information import player_statistics
 from streamlit_app.page_functions.dataset_stats import dataset_statistics
 
 path_to_teams = Path(__file__).parent.parent / "data" / "pickles" / "teams.pkl"
