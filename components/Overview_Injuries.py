d43825245d602b1bf6a1cc47cdd9a775ac2c585e
BettyJJ <245282+BettyJJ@users.noreply.github.com>
2024-03-02 09:13:21 +0100

    basic charts for injuries

--- components/Overview_Injuries.py
+++ components/Overview_Injuries.py
@@ -3,9 +3,15 @@ import streamlit as st
 from utils import Data_fetcher
 
 def overview_injuries():
-    st.write('This section will show the injuries of the players in the dataset')
-
     # fetch the data from the database
     table_name = 'injuries'
     columns = ['player_name', 'location', 'severity']
     df = Data_fetcher.fetch_from_mysql(table_name, columns)
+
+    # bar chart for location
+    st.header('Location of injuries')
+    st.bar_chart(df['location'].value_counts())
+
+    # bar chart for severity
+    st.header('Severity of injuries')
+    st.bar_chart(df['severity'].value_counts())
