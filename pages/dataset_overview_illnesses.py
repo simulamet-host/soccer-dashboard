df1fef70c6bcfb49cc3a17b6fb813391279ff74d
BettyJJ <245282+BettyJJ@users.noreply.github.com>
2024-04-04 14:53:43 +0200

    overview illnesses

--- pages/dataset_overview_illnesses.py
+++ pages/dataset_overview_illnesses.py
@@ -1,10 +1,13 @@
 import streamlit as st
 
 from components.dataset_overview import overview_common
+from components.dataset_overview import overview_illnesses
 
 def page_content():
     overview_common.page_setup()
 
     st.title('Dataset Overview - Illnesses')
+
+    overview_illnesses.overview()
 
 page_content()
--- utils/data_fetcher.py
+++ utils/data_fetcher.py
@@ -3,6 +3,16 @@ import streamlit as st
 # fetch data from mysql database and return the data
 @st.cache_data()
 def fetch_from_mysql(table: str, columns: list):
+    '''
+    Fetch data from a MySQL database and return the data as a pandas DataFrame.
+
+    Parameters:
+    table (str): name of the table to fetch data from
+    columns (list): list of columns to fetch, e.g. ['column1', 'column2'], or ['*'] to fetch all columns
+
+    Returns:
+    df (pd.DataFrame): fetched data
+    '''
     # initialize connection
     conn = st.connection('mysql', type='sql')
 
