c4394dd8af8087c058eef8007677eef88dba5969
BettyJJ <245282+BettyJJ@users.noreply.github.com>
2024-04-19 12:03:46 +0200

    put pitch image back

--- components/player_view/player_gps.py
+++ components/player_view/player_gps.py
@@ -1,8 +1,10 @@
 import altair as alt
+import pandas as pd
 import pydeck as pdk
 import streamlit as st
 
 from utils import data_fetcher
+from utils import gps
 
 def view():
     # fetch the data from the database
@@ -62,6 +64,23 @@ def altair_chart(df):
         x2='Lon_end',
         y2='Lat_end',
         color='Average_speed'
+    )
+
+    st.altair_chart(chart, use_container_width=True)
+
+    # image of football pitch
+    image_path = 'assets/pitch.png'
+    # altair can only show images in base64 format
+    image_base64 = gps.image_to_base64(image_path, image_format='PNG')
+    source = pd.DataFrame({'url': [image_base64]})
+
+    # show the image in the chart
+    # rotate the image by 90 degrees
+    chart = alt.Chart(source).mark_image(
+    ).encode(
+        url='url',
+    ).properties(
+        height=500,
     )
 
     st.altair_chart(chart, use_container_width=True)
