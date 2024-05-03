4f3a4f7ac22974b4b6bc5138be9d6363e22d4c42
BettyJJ <245282+BettyJJ@users.noreply.github.com>
2024-05-03 21:54:06 +0200

    local_coordinates_from_lat_lon

--- tests/utils/test_gps.py
+++ tests/utils/test_gps.py
@@ -18,12 +18,22 @@ def test_get_color_from_speed():
     assert gps.get_color_from_speed(7.6) == [238, 77, 90]
 
 def test_distance_and_bearing():
-    lat1, lon1, lat2, lon2 = 63.444589, 10.452373, 63.445152, 10.450687
+    lat1, lon1, lat2, lon2 = 63.445152, 10.450687, 63.444589, 10.452373
     d, b = gps.distance_and_bearing(lat1, lon1, lat2, lon2)
-    assert d == pytest.approx(104.61172932214025)
-    assert b == pytest.approx(306.7583136618076)
+    assert d == 104.61172932214025
+    assert b == 126.75680552701871
+
+    lat3, lon3 = 63.445640, 10.451500
+    d2, b2 = gps.distance_and_bearing(lat1, lon1, lat3, lon3)
+    assert d2 == 67.65938702776376
+    assert b2 == 36.677596149528995
 
 def test_local_coordinates():
     assert gps.local_coordinates(100, 0) == (0, 100)
     assert gps.local_coordinates(100, 45) == pytest.approx((70.71067811865476, 70.71067811865476))
     assert gps.local_coordinates(100, 90) == pytest.approx((100, 0))
+
+def test_local_coordinates_from_lat_lon():
+    lat1, lon1 = 63.44523733, 10.45186
+    lat2, lon2 = 63.44517933, 10.452014
+    assert gps.local_coordinates_from_lat_lon(lat1, lon1, lat2, lon2) == (41.09733383696133, 42.43931498114769, 51.08926705078824, 41.839736236557286)
